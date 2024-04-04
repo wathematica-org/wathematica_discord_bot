7395ff8642e2a2a330511fb4a7a8e1725c266103
t0d4 <todatakaaki@gmail.com>
2024-04-04 22:24:50 +0900

    fix: updated IDs for the new server

--- wathematica_discord_bot/config.py
+++ wathematica_discord_bot/config.py
@@ -12,29 +12,29 @@ class ChannelInfo(TypedDict):
 bot_name: str = "Wathematica Bot"
 category_info: dict[str, ChannelInfo] = {
     "general": {
-        "id": 1080865149902131270,
+        "id": 1218953598550020137,
         "name": "全般",
     },
     "pending_seminars": {
-        "id": 1089089108120457278,
+        "id": 1218953941245493379,
         "name": "ゼミ(仮立て)",
     },
     "ongoing_seminars": {
-        "id": 1089089162461855804,
+        "id": 1218954017980285038,
         "name": "ゼミ(本運用)",
     },
     "paused_seminars": {
-        "id": 1089089754483654687,
+        "id": 1218954072359567490,
         "name": "ゼミ(休止中)",
     },
     "finished_seminars": {
-        "id": 1089089785362120744,
+        "id": 1218954115519090850,
         "name": "ゼミ(終了)",
     },
 }
 channel_info: dict[str, ChannelInfo] = {
     "role_settings": {
-        "id": 1093502316147118140,
+        "id": 1218953566698471444,
         "name": "権限設定",
     },
 }
@@ -42,6 +42,6 @@ channel_info: dict[str, ChannelInfo] = {
 display_time_of_trivial_error: int = 30
 
 # Discord server-specific settings
-guild_id: int = 1080865149436571688
-engineer_role_id: int = 1089104583642579055
-interesting_emoji_id: int = 1093517491470352467
+guild_id: int = 1218951406128726207
+engineer_role_id: int = 1218956604968534206
+interesting_emoji_id: int = 1219276342227505202

4a06d58cebdc6ece3a8b3b35af008ed2c7502ef7
Ryosuke Yamaguchi <ryosuke_yama@RyosukenoMacBook-Air.local>
2025-04-07 23:31:11 +0900

    fix: get database url from secret

--- wathematica_discord_bot/database.py
+++ wathematica_discord_bot/database.py
@@ -1,6 +1,7 @@
 import asyncio
 import signal
 import sys
+import os
 
 from model import Base
 from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
@@ -12,7 +13,20 @@ async def create_table(engine: AsyncEngine):
 
 
 # define the engine that connects to the database
-engine = create_async_engine("sqlite+aiosqlite:///database.db")
+
+if os.path.exists("/run/secrets/database_url"):
+    with open("/run/secrets/database_url") as discord_token_file:
+        # Strip the tailing newline character with strip()
+        db_url = discord_token_file.readline().strip()
+else:
+    db_url = os.environ.get("DATABASE_URL")
+    if db_url is None:
+        raise FileNotFoundError(
+            "[NO TOKEN PROVIDED] check docker-compose.yml to see how you can expose token at /run/secrets/database_url"
+        )
+
+print("database_url", db_url)
+engine = create_async_engine(db_url)
 # define the sessionmaker that creates a "session", on which database operations are performed
 # See https://docs.sqlalchemy.org/en/20/orm/extensions/asyncio.html#synopsis-orm for further details
 async_session = async_sessionmaker(engine, expire_on_commit=False)

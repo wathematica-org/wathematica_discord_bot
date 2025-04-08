1f9e7e1b33d911cb9f4065beba165ff8f74200b2
Ryosuke Yamaguchi <ryosuke_yama@RyosukenoMacBook-Air.local>
2025-04-08 23:03:45 +0900

    fix: OB/OGサーバーへのデプロイ用に config を修正

--- wathematica_discord_bot/config.py
+++ wathematica_discord_bot/config.py
@@ -11,9 +11,9 @@ class ChannelInfo(TypedDict):
 
 # Fundamental settings
 # FIXME: set bot_name as a discord bot user name
-bot_name: str = "Wathematica Bot"
+bot_name: str = "Wathematica Bot (OB/OG)"
 # FIXME: set config_file_path as a path to config file
-config_file_path = os.path.join(os.path.dirname(__file__), "config/config-2025.json")
+config_file_path = os.path.join(os.path.dirname(__file__), "config/config-obog.json")
 
 # category_info: dict[str, ChannelInfo]
 # channel_info: dict[str, ChannelInfo]
